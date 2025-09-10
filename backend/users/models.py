1f229636a5f32d64ac13508ab2634b1963e010d3
TeoVH <villadamateo64@gmail.com>
2025-09-10 18:30:49 -0500

    refactor: move Django apps into backend folder

---
+++ backend/users/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    # Opciones para rol
+    ROLE_CHOICES = [
+        ('student', 'Student'),
+        ('admin', 'Admin'),
+    ]
+    
+    # Campos básicos
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birth_date = models.DateField(null=True, blank=True)
+    rol = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
+    
+    # Fechas automáticas
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
