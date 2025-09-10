1d4f55aac3ae02c56a9ae355a65a0f7107f1bf10
katals <juanca20042004@hotmail.com>
2025-09-10 17:48:35 -0500

    update: se añade componente sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Sidebar from './components/Sidebar/Sidebar.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="appContainer">
+      <Sidebar />
+      
+      <main className="mainContent">
+        {/* El contenido de tus páginas se renderizará aquí */}
+        <h1>Contenido Principal de la Vista</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Este es el espacio donde aparecerán todas tus diferentes páginas y
+          componentes. El sidebar se mantendrá fijo a la izquierda.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </main>
+    </div>
   )
 }
 
