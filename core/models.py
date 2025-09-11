315da4622405ef9d11ee51b85ec8c985b8e05add
Samuel <sherrerah3@eafit.edu.co>
2025-09-11 02:24:01 -0500

    CRUD notes done

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     full_name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.username
--- notequest/settings.py
+++ notequest/settings.py
@@ -123,5 +123,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.CustomUser'
+LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+# Language and timezone
+LANGUAGE_CODE = 'es-es'
+TIME_ZONE = 'America/Bogota'
